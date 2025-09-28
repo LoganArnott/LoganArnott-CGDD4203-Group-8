afd8b04cdcf79e645952f89c45be8849f00dd877
LoganArnott <larnott1@students.kennesaw.edu>
2025-09-28 14:55:11 -0400

    Player shoot script + notes in scripts

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,12 +7,16 @@ using System;
 public class PlayerController : MonoBehaviour
 {
     private Touch touch;
-    int positionCheck = 0;
     Vector2 startTouchPosition;
     Vector2 currentTouchPosition;
+    // Used to check if player is to the left/right of screen
+    int positionCheck = 0;
+    // Threshold to detect dragging touch
     float dragDistance;
+    // Used to detect if the player has already swiped before lifting their finger
     bool swiped = false;
     Camera cam;
+    // Where the player will move to
     Vector3 targetPosition;
     
     // Start is called before the first frame update
@@ -29,34 +33,44 @@ public class PlayerController : MonoBehaviour
         // Mobile Devices
         #if UNITY_IOS || UNITY_ANDROID
         
+        // Moves the player to targetPosition
         if(Mathf.Abs(transform.position.x - targetPosition.x) > 0.1f) {
             transform.position = Vector3.MoveTowards(transform.position, targetPosition, 20 * Time.deltaTime);
         } else if(Mathf.Abs(transform.position.x - targetPosition.x) <= 0.1f) {
             transform.position = targetPosition;
         }
 
+        // Checking touch
         if(Input.touchCount > 0) {
+            // Detects multiple touches
             for(int i = 0; i < Input.touchCount; i++) {
                 touch = Input.GetTouch(i);
+                // Position of touch at touch begin
                 if(touch.phase == TouchPhase.Began) {
                     startTouchPosition = Input.GetTouch(i).position;
                 }
 
+                // Tracks the current position of the player's finger
                 currentTouchPosition = Input.GetTouch(i).position;
 
+                // Swipe right 
                 if((currentTouchPosition.x - startTouchPosition.x) > dragDistance) {
                     MoveRight();
                 }
+                // Swipe left
                 if((currentTouchPosition.x - startTouchPosition.x) < (dragDistance * -1)) {
                     MoveLeft();
                 }
             }
+            // Resets swiped at touch end
             if(touch.phase == TouchPhase.Ended) {
                 swiped = false;
             }
         }
 
+        // Desktop
         #else
+
         if(Input.GetKeyDown("d")) {
             if(positionCheck != 1) {
                 Vector3 tempPosition = cam.ScreenToWorldPoint(new Vector3(Screen.width * 20 / 100, 0, 0));
@@ -75,8 +89,10 @@ public class PlayerController : MonoBehaviour
         #endif
     }
 
+    // Sets targetPosition to the right of the player
     void MoveRight()
     {
+        // Checks if the player is at the right of the screen and has already swiped before lifting their finger
         if(positionCheck != 1 && !swiped) {
             Vector3 tempPosition = cam.ScreenToWorldPoint(new Vector3(Screen.width * 20 / 100, 0, 0));
             targetPosition = new Vector3(transform.position.x - tempPosition.x, -2.5f, 0);
@@ -86,8 +102,10 @@ public class PlayerController : MonoBehaviour
         swiped = true;
     }
 
+    // Sets targetPosition to the left of the player
     void MoveLeft()
     {
+        // Checks if the player is at the left of the screen and has already swiped before lifting their finger
         if(positionCheck != -1 && !swiped) {
             Vector3 tempPosition = cam.ScreenToWorldPoint(new Vector3(Screen.width * 20 / 100, 0, 0));
             targetPosition = new Vector3((transform.position.x + tempPosition.x), -2.5f, 0);
---
+++ Assets/Scripts/PlayerShoot.cs
@@ -0,0 +1,77 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System;
+
+public class PlayerShoot : MonoBehaviour
+{
+    private Touch touch;
+    Vector2 startTouchPosition;
+    Vector2 endTouchPosition;
+    float dragDistance;
+    float startTime;
+    float endTime;
+    public GameObject bullet;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        dragDistance = Screen.width * 10 / 100;
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Mobile Devices
+        #if UNITY_IOS || UNITY_ANDROID
+
+        // Checking touch
+        if(Input.touchCount > 0) {
+            // Detects multiple touches
+            for(int i = 0; i < Input.touchCount; i++) {
+                touch = Input.GetTouch(i);
+                // Position of touch and time at touch begin
+                if(touch.phase == TouchPhase.Began) {
+                    startTouchPosition = Input.GetTouch(i).position;
+                    startTime = Time.time;
+                }
+
+                // Position of touch and time at touch end
+                if(touch.phase == TouchPhase.Ended) {
+                    endTouchPosition = Input.GetTouch(i).position;
+                    endTime = Time.time;
+                    // If its a click and not a hold/drag
+                    if((Mathf.Abs(endTouchPosition.x - startTouchPosition.x)) < dragDistance && 
+                       (endTime - startTime) < 0.5f) {
+                        Shoot();
+                    }
+                }
+            }
+        }
+
+        // Desktop
+        #else
+
+        if(Input.GetKeyDown("space")) {
+            Shoot();
+        }
+
+        #endif
+    }
+
+    // Shoots a bullet
+    void Shoot()
+    {
+        float speed = 15;
+        // Where the bullet will be created
+        Vector3 shotSpawn = new Vector3(transform.position.x, transform.position.y + 0.5f, transform.position.z);
+        
+        // Creates 1 bullet at shotSpawn
+        GameObject g = Instantiate (bullet, shotSpawn, Quaternion.identity) as GameObject;
+        Rigidbody bulletRig = g.GetComponent<Rigidbody>();
+        bulletRig.velocity = transform.up * speed;
+        Destroy (g, 0.5f);
+    }
+}
